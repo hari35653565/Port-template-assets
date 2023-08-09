dbc11f1484429d088be271dbd455cd4a751eb0a9
hari35653565 <137885952+hari35653565@users.noreply.github.com>
2023-08-09 11:56:23 +0530

    Update variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,10 +1,14 @@
 variable "resources" {
-  type = list(string)
+  type        = list(string)
   description = "List of supported resources to deploy in the stack. A resource must have a submodule with config.json, policy.json , event_rule.yaml and blueprint.tf"
 }
 
 variable "invoke_function" {
-  type = bool
-  default = true
+  type        = bool
+  default     = true
   description = "Flag to determine whether to invoke the exporter function at the end of the deployment"
+  validation {
+    condition     = var.invoke_function == true || var.invoke_function == false
+    error_message = "Invoke function must be either true or false."
+  }
 }
